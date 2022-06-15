791ef45e4b9d199c58a0fbb89672324af7a1c82d
Lenna <pampam32121@gmail.com>
2022-06-16 02:03:36 +0300

    refactored code by creating renderPosts fn

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 'use strict';
 
+let postsArr = [];
+
+function renderPosts() {
+    let html = '';
+    for (let post of postsArr) {
+        html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr>
+        `;
+    };
+    document.getElementById('blog-list').innerHTML = html;
+}
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5);
-        let html = '';
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr>
-            `;
-        };
-        document.getElementById('blog-list').innerHTML = html;
+        postsArr = data.slice(0, 5);
+        renderPosts();
     });
 
 const form = document.querySelector('#post-form');
@@ -33,11 +39,7 @@ form.addEventListener('submit', e => {
     })
         .then(res => res.json())
         .then(data => {
-            document.getElementById('blog-list').innerHTML = `
-                <h3>${data.title}</h3>
-                <p>${data.body}</p>
-                <hr>
-                ${document.getElementById('blog-list').innerHTML}
-            `
+            postsArr.unshift(data);
+            renderPosts();
         });
 })
