1701affae804bd9ae65ca8562fd63153c68e90bf
Lenna <pampam32121@gmail.com>
2022-06-15 03:10:31 +0300

    displayed the fetched data in html

--- index.js
+++ index.js
@@ -2,4 +2,15 @@
 
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(res => res.json())
-    .then(data => console.log(data.slice(0, 5)));
+    .then(data => {
+        const postsArr = data.slice(0, 5);
+        let html = '';
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr>
+            `;
+        };
+        document.getElementById('blog-list').innerHTML = html;
+    });
